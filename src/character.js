695afcb66cad0beea79585386cc490a7cad0eb81
Raphael (esarbe) Bosshard <raphael.bosshard@gmail.com>
2015-01-21 23:00:31 +0100

    finish character generator

--- src/character.js
+++ src/character.js
@@ -1,140 +1,175 @@
 var ATTR_MIN = 1;
 var ATTR_MAX = 6;
 
-Array.prototype.max = function() {
+Array.prototype.max = function () {
   return Math.max.apply(null, this);
 };
 
-Array.prototype.min = function() {
+Array.prototype.zipWithIndex = function () {
+
+	var zipped = this.map( function (elem, index) {
+	  return [elem, index];
+	});
+
+	return zipped;
+};
+
+Array.prototype.randomElement = function () {
+  return this[Math.floor(Math.random() * this.length)];
+}
+
+Array.prototype.flatten = function () {
+  var flattened = [];
+
+	return this.reduce( function (sink, elem) {
+	
+	});
+};
+
+Array.prototype.min = function () {
   return Math.min.apply(null, this);
 };
 
-var
-  UP = [1,0],
-  RIGHT = [0,1],
-  DOWN = [-1, 0],
-  LEFT = [0, -1];
+function invert (matrix) {
+	var inverted = [];
 
-var ROW = [LEFT, RIGHT];
-var COLUMN = [UP, DOWN];
+	matrix.forEach( function (row, i) {
+    row.forEach( function (cell, k) {
+			inverted[k] = inverted[k] || []
+      inverted[k][i] = matrix[i][k];
+		});	
+	});
+ 
+	return inverted;
+}
 
-function getNeighboars (matrix, cell, neighbourOffsets) {
-  var
-    x = cell[0],
-    y = cell[1];
+
+function newMatrix() {
+  return [
+		  [ 1, 1, 1 ],
+			[ 1, 1, 1 ],
+			[ 1, 1, 1 ]
+		];
+}
+
+function upperBound (row) {
+  var max = row.max();
+
+	// return the difference to the maximum value in the row
+	return row.map( function (val) {
+		return max - val;
+	});
+}
+
+// fold two matrices given the function f
+// f folds two cells
+function fold (m0, m1, f) {
+  var folded = [];
+  m0.forEach( function (row, i) {
+		folded[i] = [];
+	  row.forEach( function (cell, k) {
+		  folded[i][k] = f(m0[i][k], m1[i][k]);
+		});
+	});
+
+  return folded;
+}
+
+function cellCanBeIncreasedByOne (b, m) {
+  if (b == 0 && m < ATTR_MAX) {
+	  return true;
+	} else return (b > 1); 
+}
+
+function and (a, b) {
+  return a && b;
+}
+
+function evaluateMatrixIncrements (matrix) {
+  var ubh = matrix.map(upperBound);
+  var ubv = invert(invert(matrix).map(upperBound));
+
+  var cbiboh = fold(ubh, matrix, cellCanBeIncreasedByOne );
+  var cbibov = fold(ubv, matrix, cellCanBeIncreasedByOne );
+
+  return fold(cbiboh, cbibov, and);
+}
+
+function getValuesAndCoordinates (matrix) {
+  var cells = [];
+	matrix.forEach( function (row, index0) {
+	  row.forEach( function (cell, index1) {
+	    cells.push({value: cell, coord: [index0, index1]});
+		});
+	});
+
+  return cells;
+}
+
+function generateCharacter () {
+	var character = newMatrix();
   
-  var neighbourOffsets = neighbourOffsets || [ UP, RIGHT, DOWN, LEFT ];
-  
-  var neighbourCoordinates = neighbourOffsets.map( function (offsets) {
-   var
-      xLength = matrix.length,
-      yLength = matrix[0].length;
-    var xCoord = (offsets[0] + x + xLength) % (xLength);
-    var yCoord = (offsets[1] + y + yLength) % (yLength);
+  for (var a = 0; a < 18; a++) {
+    character = incChar(character);
+	}
 
-    return [xCoord, yCoord];
-  });
-
-  return neighbourCoordinates;
-}
-
-function getValueIn (matrix) {
-  return function (coord) { return matrix[coord[1]][coord[0]] };
-}
-
-function forCellInMatrix (matrix) { 
-  return function (f) {
-  
-    return matrix.map(function (row) {
-      return row.map( function (cell) {
-        return f(row);
-      });
-    });
-  };
-}
-
-function getDominantes (matrix) {
-  return forCellInMatrix(matrix)(isDominantIn(matrix));
-}
-
-function isDominantIn (matrix) {
-  return function (orientation) {
-	  return function (cell) {
-			return getValueIn(matrix)(cell)   
-    };
-  };
+	return character;
 }
 
 var attrs =
   [
-    [ 5, 4, 2 ],
-    [ 3, 1, 5 ],
-    [ 4, 2, 3 ],
+    [ 6, 4, 3 ],
+    [ 2, 1, 4 ],
+    [ 4, 2, 1 ],
   ];
-  
-function getDominanteValueForCellAndOrientationIn(matrix) {
-  return function (cell, orientation) {
-    var isDominant = isDominantIn(matrix)(orientation);
-    var getValue = getValueIn(matrix);
-  
-    return getNeighboars(matrix, cell, orientation).filter(isDominant).map(getValue).max();
-  };
+
+var attrs2 =
+  [
+    [ 6, 6, 6 ],
+    [ 6, 6, 6 ],
+    [ 6, 6, 6 ],
+  ];
+
+
+Character = {
+  inc: incChar,
+	build: newMatrix 
 }
 
-function getLimitsIn (matrix) {
-  var getValue = getValueIn(matrix);
-  var getDominanteValueForCellAndOrientation = getDominanteValueForCellAndOrientationIn(matrix);
-
-	return function (cell) {
-		var cellValue = getValue(cell);
-
-	  return {
-			lowerBounds: 1,
-		  upperBounds: 2
-		};
-	};
+function canIncrement (cell) {
+  return cell.value === true;
 }
 
-function getLowerBoundIn (matrix) {
-  var getValue = getValueIn(matrix);
-  var getDominanteValueForCellAndOrientation = getDominanteValueForCellAndOrientationIn(matrix);
+function copyChar (character) {
+  var copy = character.map(function (row) {
+    return row.map ( function (cell) {
+		  return cell;
+		});
+	});
 
-
-  return function (cell) {
-	};
-};
-
-function getUpperBoundIn (matrix) {
-  
-	var getValue = getValueIn(matrix);
-  var getDominanteValueForCellAndOrientation = getDominanteValueForCellAndOrientationIn(matrix);
-  
-	return function (cell) {
-
-	  var colMax = getDominanteValueForCellAndOrientation(cell, COLUMN);
-	  var rowMax = getDominanteValueForCellAndOrientation(cell, ROW);
-
-    /*if (isDominantIn(matrix)(cell)(ROW) && isDominantIn(matrix)(cell)(COLUMN)) {
-		  return ATTR_MAX; 
-		} else if (isDominantIn(matrix)(cell)(ROW)) {
-			return colMax - 1; 
-		} else if (isDominantIn(matrix)(cell)(COLUMN)) {
-		  return rowMax - 1; 
-		} else {
-		  return Math.min(rowMax, colMax) - 1;
-		}*/
-    
-	};
+	return copy;
 }
 
-function isCellDominantIn(matrix, cell) {
-  return function (orientation) {
-	  return isDominantIn(matrix)(cell)(orientation); 
-	};
+function printCharacter (character) {
+  character.forEach( function (row) {
+	  console.log(row);
+	});
 }
 
+function incChar (character ) {
+  var incrCell = getCoordsForIncr(copyChar(character)).randomElement();
+  var
+	  i = incrCell[0],
+	  k = incrCell[1];	
+	
+	character[i][k]++;    
+  return character;
+}
 
-console.log(attrs);
-console.log(forCellInMatrix(attrs)(getUpperBoundIn(attrs)));
-console.log(forCellInMatrix(attrs)(isCellDominantIn(ROW)));
+function getCoordsForIncr (matrix) {
+  return getValuesAndCoordinates(evaluateMatrixIncrements(matrix)).filter(canIncrement).map(function (cell) { return cell.coord });
+}
 
+var inc = getCoordsForIncr(attrs).randomElement()
+
+printCharacter(generateCharacter());
