62b4c582f23e6d6782a54680ffb288a47c270196
Raphael Bosshard (bra) <raphael.bosshard@imis.ch>
2015-04-10 18:37:11 +0200

    refactor to objects and methods

--- src/character.js
+++ src/character.js
@@ -1,175 +1,198 @@
+"use strict";
+
 var ATTR_MIN = 1;
 var ATTR_MAX = 6;
 
+Object.prototype.clone = function () {
+  return JSON.parse(JSON.stringify(this));
+}
+
 Array.prototype.max = function () {
   return Math.max.apply(null, this);
-};
-
-Array.prototype.zipWithIndex = function () {
-
-	var zipped = this.map( function (elem, index) {
-	  return [elem, index];
-	});
-
-	return zipped;
 };
 
 Array.prototype.randomElement = function () {
   return this[Math.floor(Math.random() * this.length)];
 }
 
-Array.prototype.flatten = function () {
-  var flattened = [];
-
-	return this.reduce( function (sink, elem) {
-	
-	});
-};
-
 Array.prototype.min = function () {
   return Math.min.apply(null, this);
 };
 
 function invert (matrix) {
-	var inverted = [];
+  var inverted = [];
 
-	matrix.forEach( function (row, i) {
+  matrix.forEach( function (row, i) {
     row.forEach( function (cell, k) {
-			inverted[k] = inverted[k] || []
+      inverted[k] = inverted[k] || [];
       inverted[k][i] = matrix[i][k];
-		});	
+    });
+  });
+
+  return inverted;
+}
+
+var Matrix = function (template) {
+
+  this.values = (template || {}).values || [
+    [ ATTR_MIN, ATTR_MIN, ATTR_MIN ],
+    [ ATTR_MIN, ATTR_MIN, ATTR_MIN ],
+    [ ATTR_MIN, ATTR_MIN, ATTR_MIN ]
+  ];
+};
+
+Matrix.prototype.invert = function () {
+  return new Matrix({values: invert(this.values)});
+}
+
+Matrix.prototype.map = function (f) {
+  return new Matrix({values: this.values.map(f)});
+}
+
+Matrix.prototype.merge = function (other, f) {
+  return new Matrix({values: merge(this.values, other.values, f)});
+}
+
+var Character = function (template) {
+  this.attributes = (template || {}).attributes || new Attributes();
+}
+
+var Attributes = function (template) {
+  Matrix.call(this, template);
+}
+
+Attributes.prototype = new Matrix();
+
+Attributes.prototype.incrementAtRandom = function () {
+
+  var incrCell = getCoordsForIncr(this).randomElement();
+
+  var values = this.values.clone();
+
+  var
+      i = incrCell[0],
+      k = incrCell[1];
+
+  values[i][k]++;
+  return new Attributes({values: values});
+}
+
+Character.generateRandom = function () {
+  var character = new Character();
+
+  for (var a = 0; a < 18; a++) {
+    character = character.incrementAttributeAtRandom();
+  }
+
+  return character;
+}
+
+Character.prototype.incrementAttributeAtRandom = function () {
+
+  var attributes = this.attributes.incrementAtRandom();
+  return new Character({attributes: attributes})
+}
+
+Character.prototype.toString = function () {
+  return JSON.stringify(this);
+}
+
+/**
+ * merge two array of arrays element wise using the given function f
+ *
+ * @param first first array of arrays to operate on
+ * @param second second array of arrays to operate on
+ * @param f function that takes two cell values and returns a new value
+ * @returns {Array}
+ */
+function merge (first, second, f) {
+  var merged = [];
+  first.forEach( function (row, i) {
+    merged[i] = [];
+	row.forEach( function (cell, k) {
+	  merged[i][k] = f(first[i][k], second[i][k]);
 	});
- 
-	return inverted;
+  });
+
+  return merged;
 }
 
-
-function newMatrix() {
-  return [
-		  [ 1, 1, 1 ],
-			[ 1, 1, 1 ],
-			[ 1, 1, 1 ]
-		];
-}
-
-function upperBound (row) {
-  var max = row.max();
-
-	// return the difference to the maximum value in the row
-	return row.map( function (val) {
-		return max - val;
-	});
-}
-
-// fold two matrices given the function f
-// f folds two cells
-function fold (m0, m1, f) {
-  var folded = [];
-  m0.forEach( function (row, i) {
-		folded[i] = [];
-	  row.forEach( function (cell, k) {
-		  folded[i][k] = f(m0[i][k], m1[i][k]);
-		});
-	});
-
-  return folded;
-}
-
-function cellCanBeIncreasedByOne (b, m) {
-  if (b == 0 && m < ATTR_MAX) {
-	  return true;
-	} else return (b > 1); 
+/**
+ * check if the given value can be incremented by one given the difference to the upper bound
+ * @param value
+ * @param differenceToUpperBound
+ * @returns {boolean} whether the value can be incremented
+ */
+function cellValueCanBeIncremented (value, differenceToUpperBound) {
+  return differenceToUpperBound > 1 || value < ATTR_MAX;
 }
 
 function and (a, b) {
   return a && b;
 }
 
-function evaluateMatrixIncrements (matrix) {
-  var ubh = matrix.map(upperBound);
-  var ubv = invert(invert(matrix).map(upperBound));
+/**
+ * map each attribute (cell) to a boolean that indicates whether it can be incremented
+ *
+ * @param attributes
+ */
+function evaluateMatrixIncrements (attributes) {
 
-  var cbiboh = fold(ubh, matrix, cellCanBeIncreasedByOne );
-  var cbibov = fold(ubv, matrix, cellCanBeIncreasedByOne );
+  var upperBoundsRowwise = attributes.map(upperBound);
+  var upperBoundsColumnwise = attributes.invert().map(upperBound).invert();
 
-  return fold(cbiboh, cbibov, and);
+  var canBeIncrementedRowwise = attributes.merge(upperBoundsRowwise, cellValueCanBeIncremented );
+  var canBeIncrementedColumnwise = attributes.merge(upperBoundsColumnwise, cellValueCanBeIncremented );
+
+  return canBeIncrementedRowwise.merge(canBeIncrementedColumnwise, and);
 }
 
-function getValuesAndCoordinates (matrix) {
-  var cells = [];
-	matrix.forEach( function (row, index0) {
-	  row.forEach( function (cell, index1) {
-	    cells.push({value: cell, coord: [index0, index1]});
-		});
-	});
+/**
+ * for every element in the array, calculate the difference to the max value in the array
+ *
+ * @param array
+ * @returns array of same length as input array
+ *   with each cell's value mapped to the difference to the maximum value in the row
+ */
+function upperBound (array) {
+  var max = array.max();
 
-  return cells;
+  return array.map( function (val) {
+    return max - val;
+  });
 }
 
-function generateCharacter () {
-	var character = newMatrix();
-  
-  for (var a = 0; a < 18; a++) {
-    character = incChar(character);
-	}
+/**
+ * returns an array of tuples with coordinates and value for each cell of the array of arrays
+ *
+ * @param arrayOfArrays
+ * @returns {Array}
+ */
+Matrix.prototype.getValuesAndCoordinates = function () {
 
-	return character;
-}
+  var mapped = this.values.map( function (row, j) {
+    return row.map(function (cell, k) {
+      return { value: cell, coord: [j, k] };
+    });
+  });
 
-var attrs =
-  [
-    [ 6, 4, 3 ],
-    [ 2, 1, 4 ],
-    [ 4, 2, 1 ],
-  ];
+  var flatMapped = [].concat.apply([], mapped);
 
-var attrs2 =
-  [
-    [ 6, 6, 6 ],
-    [ 6, 6, 6 ],
-    [ 6, 6, 6 ],
-  ];
-
-
-Character = {
-  inc: incChar,
-	build: newMatrix 
+  return flatMapped;
 }
 
 function canIncrement (cell) {
   return cell.value === true;
 }
 
-function copyChar (character) {
-  var copy = character.map(function (row) {
-    return row.map ( function (cell) {
-		  return cell;
-		});
-	});
-
-	return copy;
+function toCellCoordinate (cell) {
+  return cell.coord;
 }
 
-function printCharacter (character) {
-  character.forEach( function (row) {
-	  console.log(row);
-	});
+function getCoordsForIncr (attributes) {
+  return evaluateMatrixIncrements(attributes).getValuesAndCoordinates().filter(canIncrement).map(toCellCoordinate);
 }
 
-function incChar (character ) {
-  var incrCell = getCoordsForIncr(copyChar(character)).randomElement();
-  var
-	  i = incrCell[0],
-	  k = incrCell[1];	
-	
-	character[i][k]++;    
-  return character;
-}
-
-function getCoordsForIncr (matrix) {
-  return getValuesAndCoordinates(evaluateMatrixIncrements(matrix)).filter(canIncrement).map(function (cell) { return cell.coord });
-}
-
-var inc = getCoordsForIncr(attrs).randomElement()
-
-printCharacter(generateCharacter());
+console.log(Character.generateRandom().toString());
+//console.log(getCoordsForIncr(new Attributes()));
+//console.log(new Attributes().incrementAtRandom().values);
